87aca975b7d7b68f9302a86d0900dafa3d71fe5d
Richard Pham <richardvanpham@outlook.com>
2021-08-29 11:07:42 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+
+var exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.render("home")
+})
+
+app.listen(3000, () => {
+  console.log(`App listening on port ${port}!`)
+})
